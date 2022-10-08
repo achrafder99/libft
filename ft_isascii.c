365c7ba731bd6ec72f58e6ecd761ccfe11db7dbd
Achraf Dardour <adardour@e1r2p13.1337.ma>
2022-10-08 21:46:26 +0100

    LIBFT

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,20 +6,23 @@
 /*   By: adardour <adardour@student.1337.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 11:33:01 by adardour          #+#    #+#             */
-/*   Updated: 2022/10/08 11:52:17 by adardour         ###   ########.fr       */
+/*   Updated: 2022/10/08 21:45:09 by adardour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
-int ft_isascii(int c){
-    if(c >= 0 && c <= 127){
+int ft_isascii(int c)
+{
+    if (c >= 0 && c <= 127)
+    {
         return (1);
     }
     return (0);
 }
 
-int main(){
+int main()
+{
     printf("%d", ft_isascii('a'));
     return (0);
 }
--- ft_isprint.c
+++ ft_isprint.c
@@ -6,20 +6,23 @@
 /*   By: adardour <adardour@student.1337.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 11:52:53 by adardour          #+#    #+#             */
-/*   Updated: 2022/10/08 11:54:05 by adardour         ###   ########.fr       */
+/*   Updated: 2022/10/08 21:45:11 by adardour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
-int fft_isprint(int c){
-    if(c >= 32 && c <= 127){
+int fft_isprint(int c)
+{
+    if (c >= 32 && c <= 127)
+    {
         return (1);
     }
     return (0);
 }
 
-int main(){
+int main()
+{
     printf("%d", ft_isprint('a'));
     return (0);
 }
