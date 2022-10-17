2f2a707b6dc1cac5c6368697224da5b757cb0948
Achraf Dardour <adardour@e1r6p10.1337.ma>
2022-10-17 23:42:50 +0100

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,101 +6,101 @@
 /*   By: adardour <adardour@student.1337.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 21:34:32 by adardour          #+#    #+#             */
-/*   Updated: 2022/10/16 12:40:00 by adardour         ###   ########.fr       */
+/*   Updated: 2022/10/17 23:38:43 by adardour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int rows(char const *s, char delimiter);
-static int columnCount(char *str, char delimiter,int index);
+
+static int getRows(char const *s, char delimiter){
+    char *trim;
+    int count;
+    int OUT;
+
+    count = 0;
+    trim = ft_strtrim(s,&delimiter);
+    while (*trim != '\0')
+    {
+        if(*trim == delimiter)  OUT = 0;
+        else if(OUT == 0){
+            OUT = 1;
+            ++count;
+        }
+        ++trim;
+    }
+    return (count + 1);
+}
+static int getColumn(char *str,char delimiter,int index){
+    int length;
+    length = 0;
+
+    while (str[index] != '\0')
+    {
+        if(str[index] == delimiter)break;
+        index++;
+    }
+    return (index);
+}
 
 char **ft_split(char const *s, char c){
     char **ptr;
-    int rowcount;
-    int j;
-    int k;
-    int index;
-    int stop;
-    int aa;
-
-    rowcount = rows(s,c);
-    ptr = malloc(sizeof(char *) * rowcount + 1);
-    k = 0;
-    index = -1;
-    j = 0;
-    stop = 0;
-    aa = 0;
-    if (ptr == NULL){
-         free(ptr);
-         ptr = NULL;
-    }
-    while(rowcount > j){
-        ptr[j] = malloc(sizeof(char) * columnCount(ft_strtrim(s,&c),c,aa) + 1); // achraf| dardour
-        if(ptr[j] == NULL)
-        {
-            free(ptr[j]);
-            ptr[j] = NULL;
-        }
-        while(ft_strtrim(s,&c)[aa] != '\0' && ft_strtrim(s,&c)[aa] != c){
-            stop++;
-            aa++;
-        }
-        rowcount--;
-        j++;
-    }
-    j = 0;
-    aa = 0;
-    while (rowcount > j)
-    {
-        // Error is Here
-        while(ft_strtrim(s,&c)[aa] != '\0' && ft_strtrim(s,&c)[aa] != c){  //|achraf dardour|beerada|hfvfe
-            ptr[j][aa] = ft_strtrim(s,&c)[aa];
-            aa++;
-        }
-        ptr[j][aa] = '\0';
-        aa = aa;
-        rowcount--;
-        j++;
-    }
-
-    return ptr;
-}
-
-
-static int rows(char const *s, char delimiter)
-{
+    char *trim;
     int i;
-    int rowcount;
+    rowsandcolumn rc;
 
     i = 0;
-    rowcount = 0;
-    while (s[i + 1] != '\0')
-    {
-        if(s[i] == delimiter && s[i+1] != delimiter) rowcount++;
-        i++;
+    rc.row = getRows(s, c);
+    rc.nextBlock = 0;
+    ptr = malloc(sizeof(char*) * (rc.row + 1));
+    trim = ft_strtrim(s,&c);
+    if (ptr == NULL) return NULL;
+    while(rc.row > 0){
+        while(trim[i] != '\0'){ // achraf||||dardour||||aaaa||aaa|||sss|||||sssss
+            if(trim[i] == c) break;
+            ptr[rc.nextBlock] = (char *)malloc(sizeof(char) * (getColumn(trim, c, i) + 1));
+            i += getColumn(trim, c, i) - 1;
+            i++; 
+        }
+        if(trim[i + 1] != c){
+             rc.nextBlock++;
+             rc.row--;
+            
+        }
+      ++i;
+      rc.nextBlock = rc.nextBlock;
+      rc.row = rc.row;
     }
-    return (rowcount);
-}
+    int j = 0;
+    rc.nextBlock = 0;
+    rc.row = 3;
+    while(rc.row > 0){
+        i = j;
+        j = 0;
+        while (trim[i] != '\0')
+        {
+            if (trim[i] == c)
+                break;
+            ptr[rc.nextBlock][j] = trim[i]; // achraf||||dardour|hiba
+            if (trim[i + 1] == c)
+                ptr[rc.nextBlock][j + 1] = '\0';
+            j++;
+        }
 
-static int columnCount(char *str,char delimiter,int index){
-    int length;
-
-    length = 0;
-    while(str[index] != '\0' && str[index] != delimiter){
-        length++;
-        index++;
+        if(trim[i] == c && trim[i + 1] == c){
+            rc.nextBlock = rc.nextBlock;
+            rc.row = rc.row;
+            j++;
+        }
+        rc.nextBlock++;
+        rc.row--;
+        j += 1;
     }
-    return (length);
+    return (ptr);
 }
 
 int main(){
-    int j;
-    j = 0;
-    char **str = ft_split("|achraf dardour|beerada|hfvfe", '|');
-    while(j <= 2){
-        printf("%s\n",str[j]);
-        j++;
-    }
+    //achraf||||dardour||||aaaa||aaa|||sss|||||
+    char **res = ft_split("||||||achraf||||dardour||||aaaa||aaa|||sss|||||sssss",'|');
     return 0;
 }
